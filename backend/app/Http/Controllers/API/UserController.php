1d36d6e83d4a9341bb4360a71325613e6d33c8ca
rekto167 <ekysr99@gmail.com>
2022-04-17 04:10:42 +0700

    api logout

--- backend/app/Http/Controllers/API/UserController.php
+++ backend/app/Http/Controllers/API/UserController.php
@@ -96,4 +96,10 @@ class UserController extends Controller
         $user->update($data);
         return ResponseFormatter::success($data, 'Profile updated');
     }
+
+    public function logout(Request $request)
+    {
+        $token = $request->user()->currentAccessToken()->delete();
+        return ResponseFormatter::success($token, 'Logged out');
+    }
 }
