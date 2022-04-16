6c88ff8dbd5a3585a8ed5448f9a04bb2ed3a306a
rekto167 <ekysr99@gmail.com>
2022-04-16 11:17:52 +0700

    migration products, product galleries

--- backend/database/migrations/2022_04_16_041626_create_product_galleries_table.php
+++ backend/database/migrations/2022_04_16_041626_create_product_galleries_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductCategories extends Migration
+class CreateProductGalleriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,10 @@ class CreateProductCategories extends Migration
      */
     public function up()
     {
-        Schema::create('product_categories', function (Blueprint $table) {
+        Schema::create('product_galleries', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->bigInteger('products_id');
+            $table->string('url');
             $table->softDeletes();
             $table->timestamps();
         });
@@ -28,6 +29,6 @@ class CreateProductCategories extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('product_categories');
+        Schema::dropIfExists('product_galleries');
     }
 }
